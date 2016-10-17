b887ffdc7130d7b7763fcef107e18fef25990f9d
juliannashevchenko <juliannashevchenko@wifi-roaming-128-4-214-245.host.udel.edu>
2016-10-17 02:23:17 -0400

    it's been hours and I still don't know what's wrong...

--- Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,13 +16,31 @@ public class Staff_Test {
 
 	@Test
 	public void test() {
-		/*List <Person> StaffMembers = new ArrayList<Person>();
-		StaffMembers.add(new Staff(“Lily”, “Elizabeth”, “Jones”, 01/01/1983, “Los Angles”, “(860)-345-3437”, “lejones@udel.edu”, “Thursday 5-7pm”, 0, 27130, 09/01/2012, LADY));
-		StaffMembers.add(new Staff(“Micheal”, “Daniel”, “Stewart”, 10/10/1951, “Chicago”, “(191)-789-9999”, “mdstewart@udel.edu”, “Tuesday 2:30-4:30pm”, 0, 55000, 09/01/2013, MR));
-		StaffMembers.add(new Staff(“Charlotte”, “Ida”, “Hammond”, 06/06/1976, “New Athens”, “(765)-123-0987”, “cihammond@udel.edu”, “Wednesday 10:30am-12:30pm” , 0, 75180, 09/01/2014, DR));
-		StaffMembers.add(new Staff(“Ivan”, “Peter”, “Smith”, 09/14/1957, “Silicon Valley”, “(130)-654-1234”, “ipsmith@udel.edu”, “Thursday 2-4pm”, 0, 130000, 09/01/2015, MR));
-		StaffMembers.add(new Staff(“Kaitlyn”, “Jean”, “Neibert”, 07/21/1987, “Rohoboth”, “(890)-098-5678”, “feather@udel.edu”, “Tuesday 9:30am-11:30am”, 0, 73060, 09/01/2016, MS));*/
+		// five sample Staff instances in an ArrayList
+		ArrayList <Person> StaffMembers = new ArrayList<Person>();
+		/*StaffMembers.add(new Staff(“Lily”, “Elizabeth”, “Jones”, 01/01/1983, “Los Angles”, “(860)-345-3437”, “lejones@udel.edu”, “Thursday 5-7pm”, 0, 27130, 09/01/2012, eTitle.LADY));
+		StaffMembers.add(new Staff(“Micheal”, “Daniel”, “Stewart”, 10/10/1951, “Chicago”, “(191)-789-9999”, “mdstewart@udel.edu”, “Tuesday 2:30-4:30pm”, 0, 55000, 09/01/2013, eTitle.MR));
+		StaffMembers.add(new Staff(“Charlotte”, “Ida”, “Hammond”, 06/06/1976, “New Athens”, “(765)-123-0987”, “cihammond@udel.edu”, “Wednesday 10:30am-12:30pm” , 0, 75180, 09/01/2014, eTitle.DR));
+		StaffMembers.add(new Staff(“Ivan”, “Peter”, “Smith”, 09/14/1957, “Silicon Valley”, “(130)-654-1234”, “ipsmith@udel.edu”, “Thursday 2-4pm”, 0, 130000, 09/01/2015, eTitle.MR));
+		StaffMembers.add(new Staff(“Kaitlyn”, “Jean”, “Neibert”, 07/21/1987, “Rohoboth”, “(890)-098-5678”, “feather@udel.edu”, “Tuesday 9:30am-11:30am”, 0, 73060, 09/01/2016, eTitle.MS));*/
 		assertEquals(1,1);
 	}
+	
+	@Test
+	public void testPhoneException() {
+		// Staff instance with invalid phone number
+		ArrayList <Person> StaffMembers = new ArrayList<Person>();
+		/*StaffMembers.add(new Staff(“Leon”, “Food”, “Pears”, 11/11/1977, “Salisbury”, “1234567890”, “lfpears@udel.edu”, “Sunday 11am-8pm”, 0, 50000, 09/01/2010, eTitle.MR));*/
+		assertEquals(1,1);
+	}
+	
+	@Test
+	public void testDOBException() {
+		// Staff instance with invalid DOB
+		ArrayList <Person> StaffMembers = new ArrayList<Person>();
+		/*StaffMembers.add(new Staff(“Delia”, “Monte”, “Peaches”, 09/03/1912, “San Francisco”, “(123)-456-7890, “dmpeaches@udel.edu”, “Mondays 3-5am”, 0, 100000, 09/01/2011, eTitle.MRS));*/
+		assertEquals(1,1);
+	}
+	
 
 }
--- Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
@@ -8,6 +8,8 @@ import java.util.Calendar;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import antlr.collections.List;
+
 public class Student_Test {
 
 	@BeforeClass
@@ -15,8 +17,14 @@ public class Student_Test {
 	}
 
 	@Test
-	// need test students
 	public void test() {
+		// five sample Student instances in an ArrayList
+		ArrayList <Person> Students = new ArrayList<Person>();
+		/*Students.add(new Student(“Thomas”, “Joseph”, “Jones”, 01/01/1996, eMajor.CHEM, String Address, “(123)-456-7890”, “tjjones@udel.edu”));
+		Students.add(new Student(“Mary”, “Grace”, “Herendez”, 02/02/1997, eMajor.PHYSICS, String Address, “(456)-789-1023”, “mgherendez@udel.edu”));
+		Students.add(new Student(“Hannah”, “Starr”, “Sea”, 03/03/1998, eMajor.NURSING, String Address, “(789)-102-3456”, “hssea@udel.edu”));
+		Students.add(new Student(“Chester”, “Bona”, “Napoleon”, 04/04/1995, eMajor.PHYSICS, String Address, “(132)-546-8790”, “cbnapolean@udel.edu”));
+		Students.add(new Student(“Florence”, “Ana”, “Machine”, 05/15/1995, eMajor.COMPSI, String Address, “(546)-879-1023”, “famachine@udel.edu”));*/
 		assertEquals(1, 1);
 	}
 }
